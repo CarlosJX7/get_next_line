23ae003ded53440bcc6bdd93ab8c888a8cec96b7
CarlosJX7 <carlos.jx7@gmail.com>
2026-06-06 19:46:26 +0200

    Update C files

--- get_next_line.c
+++ get_next_line.c
@@ -18,31 +18,41 @@ char	*get_next_line(int fd)
 	static char		*remain;
 	char			*line;
 	ssize_t			bytes;
+	char			*new_line_pos;
+	char			*tmp;
 
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
-	return (NULL);
-	//bytes = read(fd, buffer, BUFFER_SIZE);
-	if (!remain)
-		remain = ft_strdup("");
+		return(NULL);
+	if (remain)
+		line = ft_strdup(remain);
 	else
-		line = ft_strjoin(remain, line);
+		line = ft_strdup("");
+	if (!line)
+	{
+		free(buffer);
+		return(NULL);
+	}
 	bytes = read(fd, buffer, BUFFER_SIZE);
 	buffer[bytes] = '\0';
 	while (bytes > 0)
 	{
 		printf("1: buffer=>%s<, line = >%s<\n", buffer, line);
+		tmp = line;
 		line = ft_strjoin(line, buffer);
+		free(tmp);
 		printf("2: buffer=>%s<, line = >%s<\n", buffer, line);
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		buffer[bytes] = '\0';
-		if (ft_strchr(buffer, '\n'))
+		new_line_pos = ft_strchr(buffer, '\n');
+		if (new_line_pos)
 		{
 			line = ft_strjoin(line, buffer);
+			remain = ft_strdup(new_line_pos + 1);
 			free(buffer);
-			return ft_substr(line, 0, ft_strchr(line, '\n') - line);
+			return ft_substr(line, 0, new_line_pos - line);
 		}
 	}
-	free(buffer);
-	return (line);
+	free(line);
+	return (NULL);
 }
